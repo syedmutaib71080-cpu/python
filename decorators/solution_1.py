fa63b71b4bdf76fc08df951b1e87991895a01cca
syedmutaib71080-cpu <syedmutaib71080@gmail.com>
2026-03-01 14:13:58 +0530

    add file and complete the time problem

--- decorators/solution_1.py
+++ decorators/solution_1.py
@@ -5,6 +5,12 @@ def timer(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Execution time: {end_time - start_time} seconds")
+        print(f" {func.__name__} ran in {end_time - start_time:.4f} seconds.")
         return result
     return wrapper
+
+@timer
+def example_function(n):
+    time.sleep(n)
+
+example_function(2)
