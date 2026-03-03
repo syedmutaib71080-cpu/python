18e9533af6a708b1f68e4cfb3d00a17129de0d96
syedmutaib71080-cpu <syedmutaib71080@gmail.com>
2026-03-03 15:00:26 +0530

    complete api

--- handling_api/freeapi_name.py
+++ handling_api/freeapi_name.py
@@ -1,13 +1,16 @@
+
 import requests
 
 def fectch_random_user_freeapi():
     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
+    response.raise_for_status()  # Check if the request was successful
     data = response.json()
     
+        
+    user_data = data["data"]
     
-    if data["success"] and "data" in data:
-        user_data = data["data"]
+    if user_data:
         username = user_data["login"]["username"]
         country = user_data["location"]["country"]
         return username, country
@@ -15,13 +18,17 @@ def fectch_random_user_freeapi():
         
     else:
         raise Exception("Failed to fetch random user data")
-    
-    def main():
-        try:
-            username, country = fectch_random_user_freeapi()
-            print(f"Username: {username}, Country: {country}")
-        except Exception as e:
-            print(str(e))
-    
-    if __name__ == "__main__":
-        main()
+
+def main():
+    print("Fetching random user data from FreeAPI...")
+
+    try:
+        username, country = fectch_random_user_freeapi()
+        print(f"Username: {username}")
+        print(f"Country: {country}")
+    except Exception as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    main()
